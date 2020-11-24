c64990560955ce6193a5c11f74e2c450fd8c7fe5
a-thoma <athoma@protonmail.com>
2020-11-24 13:24:24 -0800

    io.hpp now uses the logging mutex, so we have real ordered output

--- lib/C++/std/io.hpp
+++ lib/C++/std/io.hpp
@@ -5,37 +5,45 @@ class io
 public:
     static void println()
     {
+        std::lock_guard<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << std::endl;
     }
 
     template <typename T>
     static void println(T arg)
     {
+        std::lock_guard<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << arg << std::endl;
     }
 
     template <typename T, typename... Args>
     static void println(T arg, Args... args)
     {
+        std::unique_lock<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << arg;
+        print_lck.unlock();
         println(args...);
     }
 
     static void print()
     {
+        std::lock_guard<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << "";
     }
 
     template <typename T>
     static void print(T arg)
     {
+        std::lock_guard<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << arg;
     }
 
     template <typename T, typename... Args>
     static void print(T arg, Args... args)
     {
+        std::unique_lock<std::mutex> print_lck(pj_runtime::pj_logger::log_mtx);
         std::cout << arg;
+        print_lck.unlock();
         print(args...);
     }
 };
